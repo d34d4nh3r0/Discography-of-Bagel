b9557b4c322a39ffe5111549cdf1b2cdddf9d11b
Tallonsy <d34d4nh3r0@gmail.com>
2024-04-07 20:11:14 -0400

    Pushing v1.1.01 - see README.md

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # Discography-of-Bagel
 
 ## v1.0 - ***Plain Bagel***
-- Creates text files to be imported into FamiStudio to generate 42 second clips consisting of 256 1/4 notes based on a provided seed.
+- Creates text files to be imported into FamiStudio to generate 42-second clips consisting of 256 1/4 notes based on a provided seed.
 
 ## v1.0.1
 - added outputs directory and added outputs to gitignore, no longer machine specific
 - added four instruments to allow for instrument selection during MIDI export
-- allowed each channel to generate a seperate sequence based on inputted seed; providing 0 creates empty channel 
+- allowed each channel to generate a separate sequence based on inputted seed; providing 0 creates empty channel 
 
 ## v1.1.0 - ***Multigrain Bagel***
 - converted to modular code
 - added functionality for user to select note length instead of generating all songs as sequences of 256 1/4 beat notes
 - added functionality for randomized seed instead of user input
-- added functionality for randomized note lenght
+- added functionality for randomized note length
+
+## v1.1.01
+- added a line to view the name of the created file in the file_generation functions
+- fixed an indentation error in discography_of_bagel that created an unintended loop when specifying note length
 
 ### Roadmap to v1.2 (tentatively Asiago Bagel)
 - add functionality to select additional expansion audios for FamiStudio and generate files specific to that audio expansion
--- discography_of_bagel.py
+++ discography_of_bagel.py
@@ -4,7 +4,6 @@ import random
 import dictionaries
 import note_generation
 
-
 # Get first user decision - controlled or random generation
 note_control = user_input.ask_random("note length")
 
@@ -50,39 +49,39 @@ if note_control == "yes":
                                            square2_sequence, triangle_seed, triangle_sequence, noise_seed,
                                            noise_sequence)
 
-seed_control = user_input.ask_random("channel seeds")
-
-if seed_control == "yes":
-    square1_seed = user_input.seed_input("Square1")
-    square2_seed = user_input.seed_input("Square2")
-    triangle_seed = user_input.seed_input("Triangle")
-    noise_seed = user_input.seed_input("Noise")
 else:
-    square1_seed = random.randint(0, 18446744073709551615)
-    square2_seed = random.randint(0, 18446744073709551615)
-    triangle_seed = random.randint(0, 18446744073709551615)
-    noise_seed = random.randint(0, 18446744073709551615)
+    seed_control = user_input.ask_random("channel seeds")
 
+    if seed_control == "yes":
+        square1_seed = user_input.seed_input("Square1")
+        square2_seed = user_input.seed_input("Square2")
+        triangle_seed = user_input.seed_input("Triangle")
+        noise_seed = user_input.seed_input("Noise")
+    else:
+        square1_seed = random.randint(0, 18446744073709551615)
+        square2_seed = random.randint(0, 18446744073709551615)
+        triangle_seed = random.randint(0, 18446744073709551615)
+        noise_seed = random.randint(0, 18446744073709551615)
 
-# Generate the sequence for each channel
-generator = note_generation.Base97Generator(square1_seed)
-square1_sequence = generator.generate(2560)
+    # Generate the sequence for each channel
+    generator = note_generation.Base97Generator(square1_seed)
+    square1_sequence = generator.generate(2560)
 
-generator = note_generation.Base97Generator(square2_seed)
-square2_sequence = generator.generate(2560)
+    generator = note_generation.Base97Generator(square2_seed)
+    square2_sequence = generator.generate(2560)
 
-generator = note_generation.Base97Generator(triangle_seed)
-triangle_sequence = generator.generate(2560)
+    generator = note_generation.Base97Generator(triangle_seed)
+    triangle_sequence = generator.generate(2560)
 
-generator = note_generation.Base97Generator(noise_seed)
-noise_sequence = generator.generate(2560)
+    generator = note_generation.Base97Generator(noise_seed)
+    noise_sequence = generator.generate(2560)
 
-# Display the sequence just in case someone can read music
-print(square1_sequence)
-print(square2_sequence)
-print(triangle_sequence)
-print(noise_sequence)
+    # Display the sequence just in case someone can read music
+    print(square1_sequence)
+    print(square2_sequence)
+    print(triangle_sequence)
+    print(noise_sequence)
 
-# Pass off all the information we've gained into the file writing portion
-file_generation.chaos_file_create(square1_seed, square1_sequence, square2_seed, square2_sequence, triangle_seed,
-                                  triangle_sequence, noise_seed, noise_sequence)
+    # Pass off all the information we've gained into the file writing portion
+    file_generation.chaos_file_create(square1_seed, square1_sequence, square2_seed, square2_sequence, triangle_seed,
+                                      triangle_sequence, noise_seed, noise_sequence)
